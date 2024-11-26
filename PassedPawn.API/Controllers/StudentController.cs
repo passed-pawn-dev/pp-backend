d889280177912f20b15f137f1013a49358ea551d
tomek1421 <t.wojciechowski.648@studms.ug.edu.pl>
2024-11-26 16:31:54 +0100

    Added comments to swagger

--- PassedPawn.API/Controllers/StudentController.cs
+++ PassedPawn.API/Controllers/StudentController.cs
@@ -4,12 +4,19 @@ using PassedPawn.BusinessLogic.Services.Contracts;
 using PassedPawn.DataAccess.Repositories.Contracts;
 using PassedPawn.Models;
 using PassedPawn.Models.DTOs.User.Student;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace PassedPawn.API.Controllers;
 
 public class StudentController(IUserService userService, IUnitOfWork unitOfWork) : ApiControllerBase
 {
     [HttpPost("register")]
+    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(StudentDto))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [SwaggerOperation(
+        Summary = "Registers a new user",
+        Description = "This endpoint allows for the registration of a new user. It takes in the details of the student and creates a new record."
+    )]
     public async Task<IActionResult> Register(StudentUpsertDto studentUpsertDto)
     {
         // User service 
--- PassedPawn.API/Program.cs
+++ PassedPawn.API/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using PassedPawn.API.Extensions;
 using PassedPawn.API.Handlers;
 using PassedPawn.Models.Configuration;
@@ -13,7 +14,12 @@ builder.Services.AddApplicationServices(builder.Configuration);
 
 // Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    options.EnableAnnotations();
+});
 
 builder.Services.AddOptions<KeycloakConfig>()
     .BindConfiguration("Keycloak")
