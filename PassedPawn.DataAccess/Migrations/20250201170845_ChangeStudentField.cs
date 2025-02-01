6cca3e07578af348f6fae7f7fd3f45c3e853a633
tomek1421 <t.wojciechowski.648@studms.ug.edu.pl>
2025-02-01 18:12:34 +0100

    Rename field in puzzle entity - migrations

---
+++ PassedPawn.DataAccess/Migrations/20250201170845_ChangeStudentField.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PassedPawn.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeStudentField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PuzzleStudent_Students_StudentId",
+                table: "PuzzleStudent");
+
+            migrationBuilder.RenameColumn(
+                name: "StudentId",
+                table: "PuzzleStudent",
+                newName: "StudentsId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PuzzleStudent_StudentId",
+                table: "PuzzleStudent",
+                newName: "IX_PuzzleStudent_StudentsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PuzzleStudent_Students_StudentsId",
+                table: "PuzzleStudent",
+                column: "StudentsId",
+                principalTable: "Students",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PuzzleStudent_Students_StudentsId",
+                table: "PuzzleStudent");
+
+            migrationBuilder.RenameColumn(
+                name: "StudentsId",
+                table: "PuzzleStudent",
+                newName: "StudentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PuzzleStudent_StudentsId",
+                table: "PuzzleStudent",
+                newName: "IX_PuzzleStudent_StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PuzzleStudent_Students_StudentId",
+                table: "PuzzleStudent",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
