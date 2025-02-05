c824fb3f1cdaa2980b2bf6f31e955d85cd614871
Wikator <wiktor@szymulewicz.com>
2025-02-05 10:53:14 +0100

    Finished ClaimsPrincipalService tests

--- PassedPawn.Tests/Services/ClaimsPrincipalServiceTests.cs
+++ PassedPawn.Tests/Services/ClaimsPrincipalServiceTests.cs
@@ -1,6 +1,7 @@
 using System.Security.Claims;
 using Moq;
 using PassedPawn.BusinessLogic.Services;
+using PassedPawn.DataAccess.Entities;
 using PassedPawn.DataAccess.Repositories.Contracts;
 
 namespace PassedPawn.Tests.Services;
@@ -32,8 +33,19 @@ public class ClaimsPrincipalServiceTests
         return new ClaimsPrincipal(identity);
     }
 
+    private static Student SampleStudent()
+    {
+        return new Student
+        {
+            Id = 1,
+            Email = SampleEmail,
+            FirstName = "Student",
+            LastName = "Test"
+        };
+    }
+
     [Fact]
-    public async Task GetStudentId_ShouldReturnId_WhenStudent()
+    public async Task GetStudentId_ShouldReturnId_WhenStudentExists()
     {
         // Arrange
         const int expectedId = 1;
@@ -47,4 +59,95 @@ public class ClaimsPrincipalServiceTests
         Assert.Equal(expectedId, result);
     }
     
+    [Fact]
+    public async Task GetStudentId_ShouldThrow_WhenStudentDoesNotExist()
+    {
+        // Arrange
+        _unitOfWorkMock.Setup(unitOfWork => unitOfWork.Students.GetIdByEmail(SampleEmail))
+            .ReturnsAsync((int?)null);
+
+        // Act && Assert
+        await Assert.ThrowsAsync<Exception>(() => _claimsPrincipalService.GetStudentId(SampleClaimsPrincipal()));
+    }
+    
+    [Fact]
+    public async Task GetStudentIdOptional_ShouldReturnId_WhenStudentExists()
+    {
+        // Arrange
+        const int expectedId = 1;
+        _unitOfWorkMock.Setup(unitOfWork => unitOfWork.Students.GetIdByEmail(SampleEmail))
+            .ReturnsAsync(expectedId);
+
+        // Act
+        var result = await _claimsPrincipalService.GetStudentIdOptional(SampleClaimsPrincipal());
+
+        // Assert
+        Assert.Equal(expectedId, result);
+    }
+    
+    [Fact]
+    public async Task GetStudentIdOptional_ShouldReturnNull_WhenStudentDoesNotExist()
+    {
+        // Arrange
+        _unitOfWorkMock.Setup(unitOfWork => unitOfWork.Students.GetIdByEmail(SampleEmail))
+            .ReturnsAsync((int?)null);
+
+        // Act
+        var result = await _claimsPrincipalService.GetStudentIdOptional(SampleClaimsPrincipal());
+
+        // Assert
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public async Task GetStudent_ShouldReturnStudent_WhenStudentExists()
+    {
+        // Arrange
+        var student = SampleStudent();
+        _unitOfWorkMock.Setup(unitOfWork => unitOfWork.Students.GetUserByEmail(SampleEmail))
+            .ReturnsAsync(student);
+
+        // Act
+        var result = await _claimsPrincipalService.GetStudent(SampleClaimsPrincipal());
+
+        // Assert
+        Assert.Equal(student, result);
+    }
+    
+    [Fact]
+    public async Task GetStudent_ShouldThrow_WhenStudentDoesExist()
+    {
+        // Arrange
+        _unitOfWorkMock.Setup(unitOfWork => unitOfWork.Students.GetUserByEmail(SampleEmail))
+            .ReturnsAsync((Student?)null);
+
+        // Act && Assert
+        await Assert.ThrowsAsync<Exception>(() => _claimsPrincipalService.GetStudent(SampleClaimsPrincipal()));
+    }
+    
+    [Fact]
+    public async Task GetCoachId_ShouldReturnId_WhenStudentExists()
+    {
+        // Arrange
+        const int expectedId = 1;
+        _unitOfWorkMock.Setup(unitOfWork => unitOfWork.Coaches.GetUserIdByEmail(SampleEmail))
+            .ReturnsAsync(expectedId);
+
+        // Act
+        var result = await _claimsPrincipalService.GetCoachId(SampleClaimsPrincipal());
+
+        // Assert
+        Assert.Equal(expectedId, result);
+    }
+    
+    [Fact]
+    public async Task GetCoachId_ShouldThrow_WhenStudentDoesNotExist()
+    {
+        // Arrange
+        _unitOfWorkMock.Setup(unitOfWork => unitOfWork.Coaches.GetUserIdByEmail(SampleEmail))
+            .ReturnsAsync((int?)null);
+
+        // Act && Assert
+        await Assert.ThrowsAsync<Exception>(() => _claimsPrincipalService.GetCoachId(SampleClaimsPrincipal()));
+    }
 }
