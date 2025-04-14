d00851c471e28c661acf3786660f1613eb3ace37
Wikator <wiktor@szymulewicz.com>
2025-04-14 18:05:06 +0200

    Added Preview to Lesson

---
+++ PassedPawn.DataAccess/Migrations/20250414160229_AddPreviewToLesson.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PassedPawn.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPreviewToLesson : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Preview",
+                table: "Lessons",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Preview",
+                table: "Lessons");
+        }
+    }
+}
