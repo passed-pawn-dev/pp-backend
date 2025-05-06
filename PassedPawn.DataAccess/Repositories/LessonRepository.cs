1f737316302cfccff58653d6c998b193b9bc3d49
Wikator <wiktor@szymulewicz.com>
2025-05-06 16:03:25 +0200

    Endpoint done

--- PassedPawn.DataAccess/Repositories/LessonRepository.cs
+++ PassedPawn.DataAccess/Repositories/LessonRepository.cs
@@ -3,6 +3,7 @@ using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
 using PassedPawn.DataAccess.Entities.Courses;
 using PassedPawn.DataAccess.Repositories.Contracts;
+using PassedPawn.Models.DTOs.Course;
 using PassedPawn.Models.DTOs.Course.Lesson;
 
 namespace PassedPawn.DataAccess.Repositories;
@@ -10,14 +11,14 @@ namespace PassedPawn.DataAccess.Repositories;
 public class LessonRepository(ApplicationDbContext dbContext, IMapper mapper)
     : RepositoryBase<Lesson>(dbContext, mapper), ILessonRepository
 {
-    public async Task<IEnumerable<LessonDto>> GetUserLessons(int userId, int courseId)
+    public async Task<IEnumerable<BoughtCourseDetailsLessonDto>> GetUserLessons(int userId, int courseId)
     {
         return await DbSet
             .Include(lesson => lesson.Course)
             .ThenInclude(course => course!.Students)
             .Where(lesson =>
                 lesson.CourseId == courseId && lesson.Course!.Students.Any(student => student.Id == userId))
-            .ProjectTo<LessonDto>(MapperConfiguration)
+            .ProjectTo<BoughtCourseDetailsLessonDto>(MapperConfiguration)
             .ToListAsync();
     }
 
