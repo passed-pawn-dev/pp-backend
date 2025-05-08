fe28512480e9b46cffd01bdfd349716c8ba13240
Wikator <wiktor@szymulewicz.com>
2025-05-08 22:40:51 +0200

    Added status to Course

--- PassedPawn.Models/DTOs/Course/CourseDto.cs
+++ PassedPawn.Models/DTOs/Course/CourseDto.cs
@@ -1,3 +1,5 @@
+using PassedPawn.Models.Enums;
+
 namespace PassedPawn.Models.DTOs.Course;
 
 public class CourseDto
@@ -5,6 +7,7 @@ public class CourseDto
     public int Id { get; init; }
     public required string Title { get; init; }
     public required string Description { get; init; }
+    public CourseStatus Status { get; init; }
     public float Price { get; init; }
     public int? EloRageStart { get; init; }
     public int? EloRangeEnd { get; init; }
--- PassedPawn.Models/DTOs/Course/CourseEditViewDto.cs
+++ PassedPawn.Models/DTOs/Course/CourseEditViewDto.cs
@@ -1,3 +1,5 @@
+using PassedPawn.Models.Enums;
+
 namespace PassedPawn.Models.DTOs.Course;
 
 public class CourseEditViewDto
@@ -5,6 +7,7 @@ public class CourseEditViewDto
     public int Id { get; init; }
     public required string Title { get; init; }
     public required string Description { get; init; }
+    public CourseStatus Status { get; init; }
     public float Price { get; init; }
     public int? EloRangeStart { get; init; }
     public int? EloRangeEnd { get; init; }
