77c5eed3bd5ce15b25e36c2fc561a122e6065faa
Wikator <wiktor@szymulewicz.com>
2025-05-11 17:36:17 +0200

    Added lesson tiles to NonBoughtCourseDetailsDto

--- PassedPawn.Models/DTOs/Course/NonBoughtCourseDetailsDto.cs
+++ PassedPawn.Models/DTOs/Course/NonBoughtCourseDetailsDto.cs
@@ -20,6 +20,7 @@ public class NonBoughtCourseDetailsDto
     public double AverageScore { get; init; }
     public float Price { get; init; }
     public string? PictureUrl { get; init; }
+    public IEnumerable<NonBoughtCourseDetailsLessonDto> Lessons { get; init; } = [];
 }
 
 public class NonBoughtCourseDetailsCoachDto
@@ -29,4 +30,24 @@ public class NonBoughtCourseDetailsCoachDto
     public int CreatedCoursesCount { get; init; }
     public required string Description { get; init; }
     public string? PictureUrl { get; init; }
+}
+
+public class NonBoughtCourseDetailsLessonDto
+{
+    public int Id { get; init; }
+    public int LessonNumber { get; init; }
+    public required string Title { get; init; }
+    public bool Preview { get; init; }
+    public IEnumerable<BoughtCourseDetailsLessonElementSlimDto> Quizzes { get; init; } = [];
+    public IEnumerable<BoughtCourseDetailsLessonElementSlimDto> Puzzles { get; init; } = [];
+    public IEnumerable<BoughtCourseDetailsLessonElementSlimDto> Examples { get; init; } = [];
+    public IEnumerable<BoughtCourseDetailsLessonElementSlimDto> Videos { get; init; } = [];
+}
+
+public class NonBoughtCourseDetailsLessonElementSlimDto
+{
+    public int Id { get; init; }
+    public required string Title { get; init; }
+    public int Order { get; init; }
+    public bool Completed { get; init; }
 }
