8246334813e0419905b177da90f12f2b61f44886
Wikator <wiktor@szymulewicz.com>
2025-05-20 17:21:00 +0200

    Added FenValidation to AnswerUpsertDto

--- PassedPawn.Models/DTOs/Course/Quiz/AnswerUpsertDto.cs
+++ PassedPawn.Models/DTOs/Course/Quiz/AnswerUpsertDto.cs
@@ -1,8 +1,10 @@
-﻿namespace PassedPawn.Models.DTOs.Course.Quiz;
+﻿using PassedPawn.Models.Validators;
+
+namespace PassedPawn.Models.DTOs.Course.Quiz;
 
 public class AnswerUpsertDto
 {
     public required string Text { get; init; }
-    public string? NewPosition { get; init; }
+    [FenValidation] public string? NewPosition { get; init; }
     public string? LastMove { get; init; }
 }
