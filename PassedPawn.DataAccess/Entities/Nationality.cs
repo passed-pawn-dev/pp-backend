a68d2e941b44adacf86eaaa3889ed70b863ee70b
Wikator <wiktor@szymulewicz.com>
2025-05-22 13:26:19 +0200

    Fixed nationality fetch

--- PassedPawn.DataAccess/Entities/Nationality.cs
+++ PassedPawn.DataAccess/Entities/Nationality.cs
@@ -7,8 +7,5 @@ public class Nationality : IEntity
     [StringLength(25)] public required string FullName { get; set; }
 
     [StringLength(25)] public required string ShortName { get; set; }
-
-    public int? FlagId { get; set; }
-    public Photo? Flag { get; set; }
     public int Id { get; set; }
 }
