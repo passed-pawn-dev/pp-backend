5975da9116e1f660bb26054ad9739c56535d2e05
Krzysztof Torończak <k.toronczak.dev@gmail.com>
2025-06-10 16:18:30 +0200

    refactor: changed course example upsert to completely overwrite existing example's steps + added pgadmin to compose

---
+++ PassedPawn.DataAccess/Repositories/Contracts/ICourseExampleMoveRepository.cs
@@ -0,0 +1,8 @@
+﻿using PassedPawn.DataAccess.Entities.Courses.Elements;
+
+namespace PassedPawn.DataAccess.Repositories.Contracts;
+
+public interface ICourseExampleMoveRepository: IRepositoryBase<CourseExampleMove>
+{
+    Task<int> DeleteAllForExampleAsync(int exampleId);
+}
---
+++ PassedPawn.DataAccess/Repositories/CourseExampleMoveRepository.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PassedPawn.DataAccess.Entities.Courses.Elements;
+using PassedPawn.DataAccess.Repositories.Contracts;
+
+namespace PassedPawn.DataAccess.Repositories;
+
+public class CourseExampleMoveRepository(ApplicationDbContext dbContext, IMapper mapper)
+    : RepositoryBase<CourseExampleMove>(dbContext, mapper), ICourseExampleMoveRepository
+{
+    public async Task<int> DeleteAllForExampleAsync(int exampleId)
+    {
+        return await DbSet
+            .Where(m => m.ExampleId == exampleId)
+            .ExecuteDeleteAsync();
+    }
+}
